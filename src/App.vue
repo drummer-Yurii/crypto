3362118001f01cf6a9c40df2fe137a97cc26b3e0
Yurii Rybachok <drummer2009@ukr.net>
2024-03-23 15:51:55 +0200

    create Selector component & add styles for him

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <h1>crypto</h1>
   <TheInput />
+  <div class="selectors">
+    <TheSelector />
+    <TheSelector />
+  </div>
 </template>
 
 <script>
 import TheInput from './components/TheInput.vue';
+import TheSelector from './components/TheSelector.vue';
 export default {
   name: 'App',
   components: {
     TheInput,
+    TheSelector,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.selectors {
+  display: flex;
+  justify-content: space-around;
+  width: 700px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/TheSelector.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li>Bitcoin</li>
+    <li>ETH</li>
+    <li>USDT</li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  width: 200px;
+  padding: 0;
+  border-radius: 3px;
+  border: 3px solid #000;
+}
+
+li {
+  display: block;
+  background: #1a032d;
+  color: #fff;
+  padding: 20px 0px;
+}
+
+li:hover,
+li.active {
+  background: #24043e;
+  cursor: pointer;
+}
+</style>
