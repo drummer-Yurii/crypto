7e8f5358f6027c86b9a4cb9bddbd4045dcaf7f20
Yurii Rybachok <drummer2009@ukr.net>
2024-03-25 10:13:35 +0200

    display favorites elements

--- src/components/TheFavorite.vue
+++ src/components/TheFavorite.vue
@@ -1,11 +1,45 @@
 <template>
-  Hello
+  <h2>Options in favorites</h2>
+  <div v-for="(el, index) in favs" :key="index">
+    <span>{{ el.from }}</span>
+    <span>{{ el.to }}</span>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'TheFavorite',
+  props: {
+    favs: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+h2 {
+  color: #fff;
+}
+
+div {
+  display: flex;
+  justify-content: space-between;
+  width: 500px;
+  margin: 10px auto;
+  transition: all 500ms ease;
+}
+
+span {
+  padding: 10px 15px;
+  background: #8c00ff;
+  color: #fff;
+}
+
+div:hover {
+  opacity: 0.7;
+  transform: scale(1.1);
+  cursor: pointer;
+}
+</style>
